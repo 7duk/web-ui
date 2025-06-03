01162077d5fe8311d91ae574ce21db3c6032cce6
d2uc.11 <goline.ducnguyen@gmail.com>
2025-06-03 10:28:47 +0700

    add ssl key

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    https: {
+      key: fs.readFileSync("192.168.110.220+3-key.pem"),
+      cert: fs.readFileSync("192.168.110.220+3.pem"),
+    },
+  },
 });
